d35256c5f1ad4b38fd9a9bd25a71024f35a6f0f8
aaayush2604 <aayushfabwani2@gmail.com>
2024-08-01 11:48:09 +0530

    Deployment Chnages 1

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const cookieParser = require("cookie-parser");
 const { app, server } = require("./socket/socket.js");
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/hooks/useLogOut.jsx
+++ frontend/src/hooks/useLogOut.jsx
@@ -9,7 +9,7 @@ const useLogOut = () => {
   const logout=async()=>{
     setLoading(true);
     try{
-        const res=await fetch("/api/auth/logout",{
+        const res=await fetch("https://gup-shup-backend.vercel.app/api/auth/logout",{
             method:'POST',
             headers:{'Content-Type':'application/json'}
         });
